2843e194abfc574a3b0fb3487f743befaea0506f
Maciej Iwanowski <kasztelix@gmail.com>
2014-10-11 14:33:48 +0200

    getmypid() is not necessary anymore

--- src/Iwan/OldSchool/Process.php
+++ src/Iwan/OldSchool/Process.php
@@ -2,6 +2,7 @@
 namespace Iwan\OldSchool;
 
 use Iwan\Scrapping\Worker;
+
 /**
  * Class representing singe worker process
  *
@@ -54,7 +55,7 @@ class Process
             $end = microtime(true);
             $time = $end-$start;
             $this->queue->send(1, [$msg, $title, $time]);
-            $this->logger->debug(getmypid() . "\t$msg\t$title\t$time");
+            $this->logger->debug("$msg\t$title\t$time");
         }
     }
 }
